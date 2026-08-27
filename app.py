3ac3b24fbfc46e00ca9b41c767e4c7abfb23f87f
larissamariana-sudo <larissamariana@gmail.com>
2026-08-27 11:37:41 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ elif menu == "🎟️ Eventos e Inscrições":
     st.markdown("---")
     
     if "Jornada Científica" in evento_selecionado:
-        st.image("POTAL.png", width=400)
+        st.image("PORTAL.png", width=400)
         st.markdown("### 🩺 Jornada Científica do Curso de Fisioterapia")
         st.write("""
         * **Público-alvo:** Estudantes, docentes, profissionais e pesquisadores.
